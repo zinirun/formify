0169c0756a65ace2f52b26cdd96780cd790d945c
zinirun <heojj97@naver.com>
2021-02-09 21:00:41 +0900

    modify: imports "TypeOrmModule" and others

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { FormModule } from './form/form.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { DateScalar } from './scalars/date';
 
 @Module({
-    imports: [AuthModule],
+    imports: [
+        TypeOrmModule.forRoot(),
+        GraphQLModule.forRoot({
+            typePaths: ['./**/*.graphql'],
+            definitions: {
+                path: join(process.cwd(), 'src/autogen/schema.graphql.ts'),
+                outputAs: 'class',
+            },
+        }),
+        AuthModule,
+        UserModule,
+        FormModule,
+    ],
+    providers: [DateScalar],
 })
 export class AppModule {}
