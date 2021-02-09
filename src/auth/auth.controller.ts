cfc0851b1b08a99d880834f7f9286a0f7ed41ad6
zinirun <heojj97@naver.com>
2021-02-09 17:48:02 +0900

    modify: callbackURL "auth/"

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,17 +2,17 @@ import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 
-@Controller('google')
+@Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
-    @Get()
+    @Get('google')
     @UseGuards(AuthGuard('google'))
     async googleAuth(@Req() req) {
         console.log(req);
     }
 
-    @Get('redirect')
+    @Get('google/redirect')
     @UseGuards(AuthGuard('google'))
     googleAuthRedirect(@Req() req) {
         return this.authService.googleLogin(req);
--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -11,7 +11,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
         super({
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_SECRET,
-            callbackURL: 'http://localhost:3000/google/redirect',
+            callbackURL: 'http://localhost:3000/auth/google/redirect',
             scope: ['email', 'profile'],
         });
     }
