3e3321109e7f556f09debe1b7ca24470f2a202da
zinirun <heojj97@naver.com>
2021-02-15 10:13:28 +0900

    modify: googleLogin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Button } from 'antd';
+import { SERVER_ADDR } from './config/uri.config';
 
 function App() {
-    return <div className="App">Hello</div>;
+    const handleGoogleLogin = () => {
+        window.location.href = `${SERVER_ADDR}/auth/google`;
+    };
+    return (
+        <div>
+            <Button type="primary" onClick={handleGoogleLogin}>
+                Google Login
+            </Button>
+        </div>
+    );
 }
 
 export default App;
