34bdbc89af6dd96b02a03ddf906d8a5dd5f82ac3
zinirun <heojj97@naver.com>
2021-02-15 15:15:14 +0900

    modify: add FormDevs

--- backend/src/autogen/schema.graphql.ts
+++ backend/src/autogen/schema.graphql.ts
@@ -6,8 +6,19 @@
 
 /* tslint:disable */
 /* eslint-disable */
+export class FormInput {
+    title: string;
+    content: string;
+}
+
 export abstract class IQuery {
     abstract getUsers(): User[] | Promise<User[]>;
+
+    abstract getFormById(id?: number): Form | Promise<Form>;
+}
+
+export abstract class IMutation {
+    abstract createForm(form?: FormInput): Form | Promise<Form>;
 }
 
 export class User {
@@ -15,4 +26,14 @@ export class User {
     provider: string;
     providerId: string;
     username: string;
+    email?: string;
+}
+
+export class Form {
+    id: number;
+    title: string;
+    content: string;
+    createdAt: Date;
+    updatedAt: Date;
+    userId: number;
 }
