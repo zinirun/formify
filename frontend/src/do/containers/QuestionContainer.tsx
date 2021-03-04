020d3a63bda71ef93e38fbeaa84001c2861eb300
zinirun <heojj97@naver.com>
2021-03-04 15:37:38 +0900

    modify: configs, divide components

--- frontend/src/do/containers/QuestionContainer.tsx
+++ frontend/src/do/containers/QuestionContainer.tsx
@@ -1,36 +1,11 @@
 import React, { useEffect } from 'react';
-import { Input, Button, Radio, Checkbox, Space } from 'antd';
+import { Input, Button, Checkbox, Space } from 'antd';
 import { isMobile } from 'react-device-detect';
 import { CheckOutlined } from '@ant-design/icons';
-import { ANIMATE_DELAY, optionsStyle } from '../doConfig';
+import { ANIMATE_DELAY, optionsStyle, ALPHA_TO_KEY, ALPHA_HOTKEY } from '../doConfig';
+import { AnswerCheckbox, AnswerRadio, AnswerTextInput } from '../components/AnswerComponents';
 
 const { TextArea } = Input;
-
-const ALPHA_HOTKEY = {
-    0: 'a',
-    1: 'b',
-    2: 'c',
-    3: 'd',
-    4: 'e',
-    5: 'f',
-    6: 'g',
-    7: 'h',
-    8: 'i',
-    9: 'j',
-};
-
-const ALPHA_TO_KEY = {
-    a: 0,
-    b: 1,
-    c: 2,
-    d: 3,
-    e: 4,
-    f: 5,
-    g: 6,
-    h: 7,
-    i: 8,
-    j: 9,
-};
 
 export default function QuestionContainer({ item, inputHandler, done }) {
     useEffect(() => {
@@ -80,76 +55,25 @@ export default function QuestionContainer({ item, inputHandler, done }) {
             </div>
             <div style={{ marginBottom: '5%' }}>
                 {item.type === 'text' && (
-                    <TextArea
-                        name={item.i}
-                        onChange={handleInputChange}
-                        autoComplete="off"
-                        placeholder="여기에 입력하세요"
-                        id={`${item.i}-0`}
-                        className="form-text-input"
-                        style={{
-                            fontSize: isMobile ? 'large' : 'x-large',
-                        }}
-                        onKeyPress={(e) => handleKeyPress(e, item.link, item.i + 1)}
-                        autoSize={{
-                            maxRows: 6,
-                        }}
+                    <AnswerTextInput
+                        item={item}
+                        handleInputChange={handleInputChange}
+                        handleKeyPress={handleKeyPress}
                     />
                 )}
                 {item.type === 'selectOne' && (
-                    <Radio.Group name={item.i}>
-                        {item.options.map((option) => (
-                            <Space
-                                key={`${item.id}-${option.key}`}
-                                style={{
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    verticalAlign: 'baseline',
-                                }}
-                            >
-                                {!isMobile && (
-                                    <div className="alpha-hotkey">
-                                        {ALPHA_HOTKEY[option.key].toUpperCase()}
-                                    </div>
-                                )}
-                                <Radio
-                                    onChange={handleInputChange}
-                                    key={`${item.i}-${option.key}`}
-                                    id={`${item.i}-${option.key}`}
-                                    style={optionsStyle}
-                                    value={option.key}
-                                    onKeyPress={(e) =>
-                                        handleKeyPress(e, item.link, item.i + 1, true)
-                                    }
-                                >
-                                    {option.value}
-                                </Radio>
-                            </Space>
-                        ))}
-                    </Radio.Group>
+                    <AnswerRadio
+                        item={item}
+                        handleInputChange={handleInputChange}
+                        handleKeyPress={handleKeyPress}
+                    />
                 )}
                 {item.type === 'selectAll' && (
-                    <Checkbox.Group name={item.i} onChange={(v) => handleCheckboxChange(item.i, v)}>
-                        {item.options.map((option) => (
-                            <Space key={`${item.id}-${option.key}`} style={{ display: 'flex' }}>
-                                {!isMobile && (
-                                    <div className="alpha-hotkey">
-                                        {ALPHA_HOTKEY[option.key].toUpperCase()}
-                                    </div>
-                                )}
-                                <Checkbox
-                                    id={`${item.i}-${option.key}`}
-                                    value={option.key}
-                                    style={optionsStyle}
-                                    onKeyPress={(e) =>
-                                        handleKeyPress(e, item.link, item.i + 1, true)
-                                    }
-                                >
-                                    {option.value}
-                                </Checkbox>
-                            </Space>
-                        ))}
-                    </Checkbox.Group>
+                    <AnswerCheckbox
+                        item={item}
+                        handleInputChange={handleCheckboxChange}
+                        handleKeyPress={handleKeyPress}
+                    />
                 )}
             </div>
             <div>
--- frontend/src/do/doConfig.ts
+++ frontend/src/do/doConfig.ts
@@ -50,3 +50,29 @@ export const optionsStyle = {
 export const dropdownStyle = {
     fontSize: 'large',
 };
+
+export const ALPHA_HOTKEY = {
+    0: 'a',
+    1: 'b',
+    2: 'c',
+    3: 'd',
+    4: 'e',
+    5: 'f',
+    6: 'g',
+    7: 'h',
+    8: 'i',
+    9: 'j',
+};
+
+export const ALPHA_TO_KEY = {
+    a: 0,
+    b: 1,
+    c: 2,
+    d: 3,
+    e: 4,
+    f: 5,
+    g: 6,
+    h: 7,
+    i: 8,
+    j: 9,
+};
