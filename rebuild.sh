c64323c2e8ab3e2ed722dc63f053f71081fe4f97
zinirun <heojj97@naver.com>
2021-03-13 16:07:19 +0900

    modify: script for docker-compose

--- rebuild.sh
+++ rebuild.sh
@@ -5,4 +5,4 @@ git clone https://github.com/zinirun/formify
 cp conf/root.env formify/.env
 cp conf/backend.env formify/backend/.env
 cp conf/ormconfig.json formify/backend/
-docker-compose up -d --build
+cd formify; docker-compose up -d --build
